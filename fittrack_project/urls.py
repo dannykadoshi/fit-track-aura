b24290bb041076d06dc2fa2fac9a8247e9f79670
Danni <dannykadoshi@me.com>
2025-12-27 13:12:27 +0000

    Create homepage with hero section, features, and custom styling

--- fittrack_project/urls.py
+++ fittrack_project/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from workouts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
 
 # Serve media files in development
--- workouts/views.py
+++ workouts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def home(request):
+    """Homepage view"""
+    return render(request, 'pages/home.html')
+
+
+@login_required
+def dashboard(request):
+    """User dashboard view"""
+    context = {
+        'workouts_count': request.user.workouts.count(),
+        'recent_workouts': request.user.workouts.all()[:5],
+        'active_goals': request.user.goals.filter(is_completed=False)[:3],
+    }
+    return render(request, 'pages/dashboard.html', context)
